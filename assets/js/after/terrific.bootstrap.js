811c6a0992178e7e4180ee082fca0feea8ea827b
Simon Mollweide <simon.mollweide@namics.com>
2016-03-06 12:37:07 +0100

    outdated

--- assets/js/after/terrific.bootstrap.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-	$(document).ready(function() {
-		var $page = $('body'),
-			application = new Tc.Application();
-
-		application.registerModules($page);
-		application.start();
-	});
-})(Tc.$);
